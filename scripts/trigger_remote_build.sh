007c80905305042a6a3e9571e85ad5253a5c8434
Gonçalo <g.j.teixeiradepinhoferreira@uva.nl>
2026-05-11 14:38:00 +0200

    Changing icon size and highlight.

--- scripts/trigger_remote_build.sh
+++ scripts/trigger_remote_build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 gh repo set-default git@github.com:g-uva/goncalof.git
 gh workflow run pages.yml --ref main
+# In case you wanna check the list of last ran workflows.
+# gh run list --workflow=pages.yml
